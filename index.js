6fcad6cbdfc0ae9b41465e716e983eeb036daf66
omar issa11 <omom2168@gmail.com>
2026-06-20 16:06:58 +0300

    Refactor code structure for improved readability and maintainability

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+const app = express()
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})  
+
+const PORT = process.env.PORT || 3000
+const start = async () => {
+   try {
+     app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+     })
+   } catch (error) {
+     console.error("Error starting server:", error)
+   }
+}
+
+start() 
